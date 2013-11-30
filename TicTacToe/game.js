16e0c0a749ee027d574b4863780b140761a73b0e
Katrina Verey <kn.verey@gmail.com>
2013-11-30 02:23:04 -0500

    Outlined possible logic

--- TicTacToe/game.js
+++ TicTacToe/game.js
@@ -1,5 +1,8 @@
+// Intro screen has transparency. Offers several board sizes, or maybe a dropdown of odd numbers.
+// On hover, relevant board size displays in bkdg. On click, wrapper fades out.
+
 $(document).ready (function(){
 
-	
+
 
 });
